d70fa79a59ef9961839a1fcbb4116634192547a1
Luis Alberto <luis.pinto@smartmed.world>
2023-09-18 17:39:13 +0100

    add mongoose cfgs

--- BE/sabino-api/src/index.ts
+++ BE/sabino-api/src/index.ts
@@ -1,5 +1,8 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import mongoose from "mongoose";
+import config from "./config";
+import Express from "./config/express";
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -20,3 +23,54 @@ const typeDefs = `#graphql
     books: [Book]
   }
 `;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
+
+/**
+ * Connecting Mongoose
+ */
+mongoose.connect(config.db, {
+  keepAlive: true,
+  socketTimeoutMS: 0,
+});
+
+/**
+ * Throw error when not able to connect to database
+ */
+mongoose.connection.on("error", () => {
+  throw new Error(`unable to connect to database: ${config.db}`);
+});
