659ebf77ccd5508738fe3239a8aba0aabd2bda36
Luis Alberto <luis.pinto@smartmed.world>
2023-09-27 17:47:16 +0100

    .

--- FE/sabino-app/src/app/page.tsx
+++ FE/sabino-app/src/app/page.tsx
@@ -1,24 +1,26 @@
 "use client";
 
 import Image from "next/image";
+import React from "react";
 import { useState } from "react";
 
 export default function Home() {
-  const [todos, settodos] = useState([]);
+  const [todos, settodos] = useState("");
 
   const fetchTodos = async () => {
     console.log("Ola");
     const response = await fetch("http://localhost:8000/api/products");
-    console.log(response.json());
+    // const data = await new DefaultApi().getProductsAsync();
     const data = await response.json();
-    settodos(data);
+    console.log(data);
+    settodos(data[0].id);
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
+          Get started by editing {todos} &nbsp;
           <text>{todos}</text>
           <code className="font-mono font-bold">src/app/page.tsx</code>
           <button onClick={fetchTodos}>Get todos</button>
--- FE/sabino-app/swagger/index.ts
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-const url = "http://localhost:8000/swagger.json";
-const response = fetch(url);
-const apiDefinition = response.json();
-
-fs.writeFileSync("./api-definitions.json", JSON.stringify(apiDefinition));
