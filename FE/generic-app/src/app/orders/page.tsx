42661e8ea9307be6d22d13511402d3ac1865ddd3
AlcinoSousa <alcino.jssousa@gmail.com>
2023-10-10 23:08:04 +0100

    chore !: added product display

--- FE/generic-app/src/app/orders/page.tsx
+++ FE/generic-app/src/app/orders/page.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { DefaultApi, ProductReadDto } from "../../../apiDefinitions";
+import { Fieldset } from "primereact/fieldset";
 
 export default function ordersPage() {
-  const fetchTodos = async () => {
-    const data: ProductReadDto[] = await new DefaultApi().getProductsAsync();
-    console.log(data);
-  };
+  return <div>
+    <h1>Orders Page</h1>
+    {getProducts()}
+  </div>
+};
 
-  return <div>Orders Page</div>;
+async function getProducts() {
+  const data: ProductReadDto[] = await new DefaultApi().getProductsAsync();
+
+  return data.map(el => <Fieldset legend={el.name}>
+    <p className="m-0">
+      {el.type} at {el.createdDate.toLocaleDateString()}
+    </p>
+  </Fieldset>
+  )
 }
